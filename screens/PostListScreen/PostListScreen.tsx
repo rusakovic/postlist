9e8c07f6c3b4a17e7c31b676b6eb9545f79a6b2e
Max Rusakovič <maxrusakovic@gmail.com>
2021-10-22 18:09:00 +0200

    [4] Added post filtering

--- screens/PostListScreen/PostListScreen.tsx
+++ screens/PostListScreen/PostListScreen.tsx
@@ -2,7 +2,7 @@ import ContainerCenter from 'components/atoms/Containers/ContainerCenter'
 import ContainerSpace from 'components/atoms/Containers/ContainerSpace'
 import DefaultText from 'components/atoms/Text/DefaultText/DefaultText'
 import { PostPreview } from 'components/molecules'
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import {
   ActivityIndicator,
   FlatList,
@@ -14,7 +14,6 @@ import { useFetchData } from 'utils/fetchData/fetchDataHook'
 import { PostListScreenStyles } from './styles'
 import { Feather as Icon } from '@expo/vector-icons'
 import styled from 'constants/styled'
-import ButtonWithShadowSmall from 'components/atoms/Buttons/ButtonWithShadowSmall'
 
 interface PostListScreenProps {}
 
@@ -22,61 +21,72 @@ const PostListScreen: React.FunctionComponent<PostListScreenProps> = (
   props
 ) => {
   const { posts, error, isLoading } = useFetchData()
+
   const [searchText, setSearchText] = useState('')
+
+  const filterPostsByUserId = () =>
+    isSearchTextFilled
+      ? posts.filter((item) => item.userId === +searchText)
+      : posts
+
+  const isSearchTextFilled = searchText.length
+
+  const filteredPosts = useMemo(
+    () => filterPostsByUserId(),
+    [searchText, posts]
+  )
+
   const onClearSearchInputHandler = () => {
     setSearchText('')
   }
-  const isSearchTextFilled = searchText.length
   console.log('🚀 ~ file: PostListScreen.tsx ~ line 12 ~ posts', posts)
-  return useMemo(
-    () => (
-      <>
-        <View style={PostListScreenStyles.searchWrapper}>
-          <View style={PostListScreenStyles.searchInput}>
-            <TextInput
-              value={searchText}
-              placeholder='search...'
-              style={PostListScreenStyles.searchField}
-              onChangeText={setSearchText}
-            />
-            {isSearchTextFilled ? (
-              <Pressable
-                style={PostListScreenStyles.crossButtonWrapper}
-                onPress={onClearSearchInputHandler}
-              >
-                <Icon
-                  name='x-circle'
-                  size={20}
-                  color={styled.colors.grey30opacity}
-                />
-              </Pressable>
-            ) : (
-              <></>
-            )}
-          </View>
-        </View>
-        {isLoading ? (
-          <ContainerCenter isMarginTop>
-            <ContainerSpace mtXL />
-            <ActivityIndicator size='large' />
-          </ContainerCenter>
-        ) : error ? (
-          <ContainerCenter isContainer isVerticalCenter>
-            <DefaultText numberOfLines={4}>Error occurs: {error}</DefaultText>
-          </ContainerCenter>
-        ) : (
-          <FlatList
-            data={posts}
-            keyExtractor={({ id }) => id.toString()}
-            renderItem={({ item: { userId, title } }) => {
-              return <PostPreview userId={userId} title={title} />
-            }}
-            ListFooterComponent={<ContainerSpace mtXL />}
+  return (
+    <>
+      <View style={PostListScreenStyles.searchWrapper}>
+        <View style={PostListScreenStyles.searchInput}>
+          <TextInput
+            value={searchText}
+            placeholder='search...'
+            style={PostListScreenStyles.searchField}
+            onChangeText={setSearchText}
+            keyboardType='numeric'
           />
-        )}
-      </>
-    ),
-    [posts, error, isLoading, searchText]
+          {isSearchTextFilled ? (
+            <Pressable
+              style={PostListScreenStyles.crossButtonWrapper}
+              onPress={onClearSearchInputHandler}
+            >
+              <Icon
+                name='x-circle'
+                size={20}
+                color={styled.colors.grey30opacity}
+              />
+            </Pressable>
+          ) : (
+            <></>
+          )}
+        </View>
+      </View>
+      {isLoading ? (
+        <ContainerCenter isMarginTop>
+          <ContainerSpace mtXL />
+          <ActivityIndicator size='large' />
+        </ContainerCenter>
+      ) : error ? (
+        <ContainerCenter isContainer isVerticalCenter>
+          <DefaultText numberOfLines={4}>Error occurs: {error}</DefaultText>
+        </ContainerCenter>
+      ) : (
+        <FlatList
+          data={filteredPosts}
+          keyExtractor={({ id }) => id.toString()}
+          renderItem={({ item: { userId, title } }) => {
+            return <PostPreview userId={userId} title={title} />
+          }}
+          ListFooterComponent={<ContainerSpace mtXL />}
+        />
+      )}
+    </>
   )
 }
 
